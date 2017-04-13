e743fd12ba18650d60c74b79a518a56c0a101b0b
zxd0922 <442421993@qq.com>
2017-04-13 10:49:00 +0800

    Initial Commit

---
+++ Runtime练习/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Runtime练习
+//
+//  Created by 李清娟 on 2017/4/13.
+//  Copyright © 2017年 --. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Runtime练习/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Runtime练习
+//
+//  Created by 李清娟 on 2017/4/13.
+//  Copyright © 2017年 --. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
